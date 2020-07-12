c9ff8870abe58c1c904a2e6f8126b52e84d360e9
Mohamed <mohammed.korany.ali@gmail.com>
2020-07-12 21:04:08 +0200

    Initial Commit

---
+++ MVVM_InPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM_InPractice
+//
+//  Created by Mohamed Korany Ali on 7/12/20.
+//  Copyright © 2020 Mohamed Korany Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
